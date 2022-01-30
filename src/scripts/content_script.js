8978608e7e36e8288f2db603ba02c7f8aeee15b7
Davide Ciulla <davide.ciulla@hotmail.com>
2022-01-30 22:07:26 +0100

    Update

--- src/scripts/content_script.js
+++ src/scripts/content_script.js
@@ -1,32 +1,23 @@
-const onReady = (fn) => {
+const onReady = (callback) => {
 	if (document.readyState != 'loading') {
-		setTimeout(fn, 1000);
+		setTimeout(callback, 1000);
 	} else {
-		document.addEventListener('DOMContentLoaded', fn);
+		document.addEventListener('DOMContentLoaded', callback);
 	}
 };
 
-const slugify = (text) =>
-	text
-		.split(/[^A-Za-z]/g)
-		.map((el) => el.toLowerCase())
-		.map((el, idx) => {
-			if (idx === 0) return el;
-			return el[0].toUpperCase() + el.slice(1);
-		})
-		.join('');
-
-onReady(() => {
+const extractFontData = () => {
 	const title = document.querySelector('div.sticky-header h1').textContent;
 	const variants = document.querySelectorAll('span.variant__style');
-
-	const typeface = {
+	return {
 		family: title,
 		slug: slugify(title),
 		variants: [...variants].map((variant) => variant.textContent.trim()),
 		url: document.location.href
 	};
+};
 
+const injectStyles = () => {
 	const head = document.querySelector('head');
 	const stylesheet = document.createElement('style');
 	stylesheet.setAttribute('data-extension', 'google-fonts-collections');
@@ -62,7 +53,7 @@ onReady(() => {
 			height: 20px;
 			display: flex;
 			align-items: center;
-			transform = translateY(-1px);
+			transform: translateY(-1px);
 		}
 		
 		.button__addToCollection:hover {
@@ -72,72 +63,91 @@ onReady(() => {
 		}
 	`;
 	head.appendChild(stylesheet);
+};
 
+const updateButton = (button, icon, fontInFavorites, callback = () => null) => {
+	button.classList[fontInFavorites ? 'add' : 'remove']('active');
+	button.innerText = fontInFavorites ? 'Remove from collection' : 'Add to collection';
+	icon.textContent = fontInFavorites ? '-' : '+';
+	button.appendChild(icon);
+	callback();
+};
+
+const createButton = () => {
 	const button = document.createElement('button');
 	button.innerText = 'Add to collection';
 	button.classList.add('button__addToCollection');
-
-	document.addEventListener('scroll', () => {
-		if (window.scrollY > 130) {
-			button.classList.add('collapsed-header');
-		} else {
-			button.classList.remove('collapsed-header');
-		}
-	});
 
 	const icon = document.createElement('span');
 	icon.textContent = '+';
 	button.appendChild(icon);
 
+	return [button, icon];
+};
+
+const placeButtonOnScreen = (button) => {
+	const downloadButtonStd = document.querySelector('button.sticky-header__cta-button');
+	downloadButtonStd.insertAdjacentElement('beforeBegin', button);
+};
+
+const injectMarkup = (typeface) => {
+	const [button, icon] = createButton();
+
+	// Fix button style when placed in collapsed header
+	document.addEventListener('scroll', () => {
+		button.classList[window.scrollY > 130 ? 'add' : 'remove']('collapsed-header');
+	});
+
 	// Check if page font is in favorites
 	chrome.storage.sync.get('favorites', ({ favorites }) => {
 		const fav = new Map(favorites);
-
-		if (fav.has(typeface.slug)) {
-			button.classList.add('active');
-			button.innerText = 'Remove from collection';
-			icon.textContent = '-';
-			button.appendChild(icon);
-		} else {
-			button.classList.remove('active');
-			button.innerText = 'Add to collection';
-			icon.textContent = '+';
-			button.appendChild(icon);
-		}
+		const fontInFavorites = fav.has(typeface.slug);
+		updateButton(button, icon, fontInFavorites);
 	});
 
-	// Deal
+	// React to click events on new added button
 	button.addEventListener('click', () => {
 		chrome.storage.sync.get('favorites', ({ favorites }) => {
 			const fav = new Map(favorites);
-
-			if (!fav.has(typeface.slug)) {
-				console.log('adding to favorites');
-				fav.set(typeface.slug, typeface);
-				button.classList.add('active');
-				button.innerText = 'Remove from collection';
-				icon.textContent = '-';
-				button.appendChild(icon);
-			} else {
-				console.log('removing from favorites');
-				fav.delete(typeface.slug);
-				button.classList.remove('active');
-				button.innerText = 'Add to collection';
-				icon.textContent = '+';
-				button.appendChild(icon);
-			}
-
-			const updated = Array.from(fav);
-			chrome.storage.sync.set({ favorites: updated });
+			const fontInFavorites = fav.has(typeface.slug);
+			updateButton(button, icon, !fontInFavorites, () => {
+				if (!fontInFavorites) {
+					console.log(typeface);
+					typeface['added_at'] = Date.now();
+					console.log(typeface);
+					fav.set(typeface.slug, typeface);
+				} else {
+					fav.delete(typeface.slug);
+				}
+			});
+			chrome.storage.sync.set({ favorites: Array.from(fav) });
 		});
 	});
 
-	const downloadButtonStd = document.querySelector(
-		'button.sticky-header__cta-button'
-	);
-	downloadButtonStd.insertAdjacentElement('beforeBegin', button);
-
-	chrome.storage.sync.get(null, function (data) {
-		console.info('Storage:', data);
+	// Update button when extension removes font
+	chrome.runtime.onMessage.addListener((request, sendResponse) => {
+		const canUpdateButton = request.message === 'removed-font' && request.font === typeface.slug;
+		updateButton(button, icon, !canUpdateButton);
 	});
+
+	placeButtonOnScreen(button);
+};
+
+const slugify = (text) =>
+	text
+		.split(/[^A-Za-z]/g)
+		.map((el) => el.toLowerCase())
+		.map((el, idx) => {
+			if (idx === 0) return el;
+			return el[0].toUpperCase() + el.slice(1);
+		})
+		.join('');
+
+onReady(() => {
+	injectStyles();
+	injectMarkup(extractFontData());
+
+	// chrome.storage.sync.get(null, function (data) {
+	// 	console.info('Storage:', data);
+	// });
 });
--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,39 +1,68 @@
-const container = document.getElementById('favorites');
+const sortBySlug = (a, b) => {
+	if (a[1].slug < b[1].slug) return -1;
+	if (a[1].slug > b[1].slug) return 1;
+	return 0;
+};
+
+const injectMarkup = ({ variants, family, url, slug }) => {
+	const container = document.getElementById('fonts');
+	const font = document.createElement('div');
+	font.classList.add('font');
+	font.classList.add(`font-${slug}`);
+	font.innerHTML = `
+		<div>
+			<a href="${url}" target="_blank">${family}</a>
+			<p>${variants.length} variant${variants.length > 1 ? 's' : ''}</p>
+		</div>
+		<div class="remove">
+			<svg xmlns="http://www.w3.org/2000/svg" width="192" height="192" fill="#000000" viewBox="0 0 256 256">
+				<rect width="256" height="256" fill="none"></rect>
+				<line x1="200" y1="56" x2="56" y2="200" stroke="#000000" stroke-linecap="round" stroke-linejoin="round"
+					stroke-width="16"></line>
+				<line x1="200" y1="200" x2="56" y2="56" stroke="#000000" stroke-linecap="round" stroke-linejoin="round"
+					stroke-width="16"></line>
+			</svg>
+		</div>
+	`;
+	container.appendChild(font);
+};
 
 chrome.storage.sync.get('favorites', ({ favorites }) => {
+	console.log(favorites);
+
 	if (favorites === undefined || favorites.length === 0) {
 		const noFonts = document.getElementById('no-fonts');
 		noFonts.classList.remove('hidden');
 	} else {
-		favorites
-			.sort((a, b) => {
-				console.log(a, b);
-				if (a[1].slug < b[1].slug) return -1;
-				if (a[1].slug > b[1].slug) return 1;
-				return 0;
-			})
-			.forEach((favorite) => {
-				console.log(favorite[1]);
-				const { slug, family, variants, url } = favorite[1];
-				const font = document.createElement('div');
-				font.classList.add('font');
-				font.innerHTML = `
-				<div>
-					<a href="${url}" target="_blank">${family}</a>
-					<p>${variants.length} variant${variants.length > 1 ? 's' : ''}</p>
-				</div>
-				<button id="remove-${slug}">Remove</button>
-			`;
-				container.appendChild(font);
-				const removeBtn = document.getElementById(`remove-${slug}`);
-				removeBtn.addEventListener('click', () => {
-					console.log('click');
-					const fav = new Map(favorites);
-					fav.delete(slug);
-					const updated = Array.from(fav);
-					console.log(updated);
-					chrome.storage.sync.set({ favorites: updated });
+		favorites.sort(sortBySlug).forEach((favorite) => {
+			const { slug } = favorite[1];
+			injectMarkup(favorite[1]);
+
+			const removeBtn = document.querySelector(`.font-${slug} .remove`);
+			removeBtn.addEventListener('click', () => {
+				font.style.display = 'none';
+				const fav = new Map(favorites);
+				fav.delete(slug);
+				chrome.storage.sync.set({ favorites: Array.from(fav) });
+
+				if (fav.size === 0) {
+					const noFonts = document.getElementById('no-fonts');
+					noFonts.classList.remove('hidden');
+				}
+
+				chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+					chrome.tabs.sendMessage(tabs[0].id, {
+						message: 'removed-font',
+						font: slug
+					});
 				});
 			});
+		});
 	}
+});
+
+const search = document.querySelector('input');
+console.log(search);
+search.addEventListener('change', (e) => {
+	console.log(e.target.value);
 });
