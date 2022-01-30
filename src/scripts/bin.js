5639a0ffe7f10b01e9ff3a0f6552d3142ba09efb
Davide Ciulla <davide.ciulla@hotmail.com>
2022-01-30 11:27:33 +0100

    Reviewed project structure

--- src/scripts/bin.js
+++
@@ -1,9 +0,0 @@
-let previousUrl = '';
-const observer = new MutationObserver(function (mutations) {
-	if (document.location.href !== previousUrl) {
-		previousUrl = document.location.href;
-		console.log(`URL changed to ${document.location.href}`);
-	}
-});
-const config = { subtree: true, childList: true };
-observer.observe(document, config);
