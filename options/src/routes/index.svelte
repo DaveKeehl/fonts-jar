479748109f986bdbc89d711d30e739f95f583737
Davide Ciulla <davide.ciulla@hotmail.com>
2022-02-05 23:44:08 +0100

    Tried to add SvelteKit for the options page

---
+++ options/src/routes/index.svelte
@@ -0,0 +1,16 @@
+<script>
+	async function getStorageValuePromise(key) {
+		return new Promise((resolve) => {
+			chrome.storage.sync.get(key, resolve);
+		});
+	}
+
+	const promise = getStorageValuePromise('favorites')
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+{#await promise then value}
+	<p>{value}</p>
+{/await}
