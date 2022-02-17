90d7efe6a370ddf28c27705e523a9f68e12633a0
Davide Ciulla <davide.ciulla@hotmail.com>
2022-02-17 11:08:17 +0100

    Playing with some listeners

--- src/background/index.js
+++ src/background/index.js
@@ -1,3 +1,9 @@
 // chrome.runtime.onInstalled.addListener(() => {
 // 	chrome.storage.sync.set({ favorites: [] });
 // });
+
+chrome.tabs.onActivated.addListener((activeInfo) => {
+	console.log(activeInfo);
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => console.log(tabId, changeInfo, tab));
