56eeba2a3168f5f96ccd83f07e089a1098d4b4e6
Davide Ciulla <davide.ciulla@hotmail.com>
2023-01-06 23:18:04 +0100

    Added docs

--- popup/utils.ts
+++ popup/utils.ts
@@ -1,18 +1,36 @@
 import type { CompareFunction, ISorting } from "~types/sorting"
 import type { TypefaceTuple } from "~types/typeface"
 
+/**
+ * Compare function that allows to sort typefaces by slug in ascending order.
+ * @param a - The first typeface to compare.
+ * @param b - The second typeface to compare.
+ * @returns Whether the first slug either came first, last or was the same as the other one.
+ */
 const sortAscendingBySlug = (a: TypefaceTuple, b: TypefaceTuple) => {
   if (a[1].slug < b[1].slug) return -1
   if (a[1].slug > b[1].slug) return 1
   return 0
 }
 
+/**
+ * Compare function that allows to sort typefaces by slug in descending order.
+ * @param a - The first typeface to compare.
+ * @param b - The second typeface to compare.
+ * @returns Whether the first slug either came first, last or was the same as the other one.
+ */
 const sortDescendingBySlug = (a: TypefaceTuple, b: TypefaceTuple) => {
   if (a[1].slug > b[1].slug) return -1
   if (a[1].slug < b[1].slug) return 1
   return 0
 }
 
+/**
+ * Compare function that allows to sort typefaces by date in ascending order.
+ * @param a - The first typeface to compare.
+ * @param b - The second typeface to compare.
+ * @returns Whether the first date either came first, last or was the same as the other one.
+ */
 const sortAscendingByDate = (a: TypefaceTuple, b: TypefaceTuple) => {
   const aDate = new Date(a[1].added_at)
   const bDate = new Date(b[1].added_at)
@@ -22,6 +40,12 @@ const sortAscendingByDate = (a: TypefaceTuple, b: TypefaceTuple) => {
   return 0
 }
 
+/**
+ * Compare function that allows to sort typefaces by date in descending order.
+ * @param a - The first typeface to compare.
+ * @param b - The second typeface to compare.
+ * @returns Whether the first date either came first, last or was the same as the other one.
+ */
 const sortDescendingByDate = (a: TypefaceTuple, b: TypefaceTuple) => {
   const aDate = new Date(a[1].added_at)
   const bDate = new Date(b[1].added_at)
@@ -31,6 +55,11 @@ const sortDescendingByDate = (a: TypefaceTuple, b: TypefaceTuple) => {
   return 0
 }
 
+/**
+ * Given some sorting information (method and direction), return the appropriate compare function to be used.
+ * @param sort - The sorting information (method and direction).
+ * @returns The appropriate compare function.
+ */
 export const getSortFunction = (sort: ISorting) => {
   const { method, direction } = sort
   let sortFunction: CompareFunction | string = ""
