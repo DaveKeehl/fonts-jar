88d5c5c09472d3420c156a4d61952052eb2444bf
Davide Ciulla <davide.ciulla@hotmail.com>
2023-01-08 00:35:02 +0100

    More permissive filtering

--- popup/components/Favorites.tsx
+++ popup/components/Favorites.tsx
@@ -4,19 +4,29 @@ import { Favorite } from "./Favorite"
 import { NothingToShow } from "./NothingToShow"
 
 import { getSortFunction } from "../utils"
-import type { ISorting } from "~types/sorting"
+import type { ISorting } from "types/sorting"
+import type { TypefaceTuple } from "types/typeface"
 
 export const Favorites = () => {
   const [searchQuery] = useStorage("searchQuery", "")
-  const [favorites] = useStorage("favorites", [])
+  const [favorites] = useStorage<TypefaceTuple[]>("favorites", [])
   const [method] = useStorage<ISorting["method"]>("sortMethod", "alphabetical")
   const [direction] = useStorage<ISorting["direction"]>("sortDirection", "ascending")
 
-  const results = [...favorites]
-    .sort(getSortFunction({ method, direction }))
-    .filter((favorite) =>
-      favorite[1].family.toLowerCase().includes(searchQuery.trim().toLowerCase())
-    )
+  const results = [...favorites].sort(getSortFunction({ method, direction })).filter((favorite) => {
+    const cleanQuery = searchQuery.trim().toLowerCase()
+    const { family, origin } = favorite[1]
+
+    const familyNormalized = family.toLowerCase()
+    const originNormalized = origin.name.toLowerCase()
+
+    const familyContainsQuery = familyNormalized.includes(cleanQuery)
+    const originContainsQuery = originNormalized.includes(cleanQuery)
+    const queryContainsFamily = cleanQuery.includes(familyNormalized)
+    const queryContainsOrigin = cleanQuery.includes(originNormalized)
+
+    return familyContainsQuery || originContainsQuery || queryContainsFamily || queryContainsOrigin
+  })
 
   if (favorites.length === 0) {
     return <NothingToShow>No fonts added</NothingToShow>
