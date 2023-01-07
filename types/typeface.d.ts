167329d248c06e3c9fb6631c561e1e50f7dd440b
Davide Ciulla <davide.ciulla@hotmail.com>
2023-01-08 00:34:49 +0100

    Fixed type names and imports

--- types/typeface.d.ts
+++ types/typeface.d.ts
@@ -1,16 +1,16 @@
-import { SupportedWebsite } from "../types"
+import { SupportedWebsite } from "./website"
 
 export interface ITypeface {
   family: string
   slug: string
   styles: string[]
   variableAxes: number
-  origin: TypefaceOrigin
+  origin: ITypefaceOrigin
   added_at: string
   collections: string[]
 }
 
-export type TypefaceTuple = [string, Typeface] // [<typeface_name>, <typeface_data>]
+export type TypefaceTuple = [string, ITypeface] // [<typeface_name>, <typeface_data>]
 
 export interface ITypefaceOrigin {
   name: SupportedWebsite
