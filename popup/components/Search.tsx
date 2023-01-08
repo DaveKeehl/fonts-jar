cb0e800fa1ec8e73bbf7e5001b1c9a279d5b3f53
Davide Ciulla <davide.ciulla@hotmail.com>
2023-01-08 15:08:09 +0100

    Inlined event handler

--- popup/components/Search.tsx
+++ popup/components/Search.tsx
@@ -1,14 +1,8 @@
-import type React from "react"
 import { MagnifyingGlass } from "phosphor-react"
-
 import { useStorage } from "@plasmohq/storage/hook"
 
 export const Search = () => {
   const [searchQuery, setSearchQuery] = useStorage("searchQuery", "")
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchQuery(e.target.value)
-  }
 
   return (
     <div className="search relative flex h-full flex-1 flex-row items-center">
@@ -19,7 +13,7 @@ export const Search = () => {
         autoComplete="off"
         className="h-full w-full border-none py-0 pr-4 pl-10 font-poppins text-base text-greyscale-900 [background:none] placeholder:text-greyscale-600 focus:outline-0"
         value={searchQuery}
-        onChange={handleChange}
+        onChange={(e) => setSearchQuery(e.target.value)}
       />
     </div>
   )
